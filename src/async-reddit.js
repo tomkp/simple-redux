5250fbde9aad5d3e27ec57496430ba3a68ef7761
tomkp <tom@tomkp.com>
2016-06-21 16:40:40 +0100

    ...

--- src/async-reddit.js
+++ src/async-reddit.js
@@ -46,37 +46,47 @@ const receivePosts = (subreddit, json) => {
     }
 };
 
-// Meet our first thunk action creator!
-// Though its insides are different, you would use it just like any other action creator:
-// store.dispatch(fetchPosts('reactjs'))
+
+//............
+
 const fetchPosts = (subreddit) => {
-
-    // Thunk middleware knows how to handle functions.
-    // It passes the dispatch method as an argument to the function,
-    // thus making it able to dispatch actions itself.
-    return (dispatch) => {
-
-        // First dispatch: the app state is updated to inform
-        // that the API call is starting.
+    return dispatch => {
         dispatch(requestPosts(subreddit));
-
-        // The function called by the thunk middleware can return a value,
-        // that is passed on as the return value of the dispatch method.
-        // In this case, we return a promise to wait for.
-        // This is not required by thunk middleware, but it is convenient for us.
         return fetch(`http://www.reddit.com/r/${subreddit}.json`)
             .then(response => response.json())
-            .then(json =>
-                // We can dispatch many times!
-                // Here, we update the app state with the results of the API call.
-                dispatch(receivePosts(subreddit, json))
-            )
-            ;
-        // In a real world app, you also want to
-        // catch any error in the network call.
+            .then(json => dispatch(receivePosts(subreddit, json)))
     }
 };
 
+const shouldFetchPosts = (state, subreddit) => {
+    const posts = state.postsBySubreddit[subreddit];
+    if (!posts) {
+        return true
+    } else if (posts.isFetching) {
+        return false
+    } else {
+        return posts.didInvalidate
+    }
+};
+
+const fetchPostsIfNeeded = (subreddit) => {
+
+    // Note that the function also receives getState()
+    // which lets you choose what to dispatch next.
+
+    // This is useful for avoiding a network request if
+    // a cached value is already available.
+
+    return (dispatch, getState) => {
+        if (shouldFetchPosts(getState(), subreddit)) {
+            // Dispatch a thunk from thunk!
+            return dispatch(fetchPosts(subreddit))
+        } else {
+            // Let the calling code know there's nothing to wait for.
+            return Promise.resolve()
+        }
+    }
+};
 //................................
 
 // reducers
@@ -153,7 +163,10 @@ const store = createStore(
 );
 
 store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(fetchPosts('reactjs')).then(() =>
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() =>
+    console.log(store.getState())
+);
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() =>
     console.log(store.getState())
 );
 
