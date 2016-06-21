7a709660ea1cfa2e6629115c0c199a2954ca3643
tomkp <tom@tomkp.com>
2016-06-21 16:31:02 +0100

    async reddit

---
+++ src/async-reddit.js
@@ -0,0 +1,186 @@
+const {createStore, applyMiddleware, combineReducers} = require('redux');
+
+const thunk = require('redux-thunk').default;
+const createLogger = require('redux-logger');
+const fetch = require('isomorphic-fetch');
+
+require ('babel-polyfill');
+
+
+
+const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
+
+function selectSubreddit(subreddit) {
+    return {
+        type: SELECT_SUBREDDIT,
+        subreddit
+    }
+}
+const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
+
+function invalidateSubreddit(subreddit) {
+    return {
+        type: INVALIDATE_SUBREDDIT,
+        subreddit
+    }
+}
+
+
+
+const REQUEST_POSTS = 'REQUEST_POSTS'; 
+function requestPosts(subreddit) {
+    return {
+        type: REQUEST_POSTS,
+        subreddit
+    }
+}
+
+const RECEIVE_POSTS = 'RECEIVE_POSTS';
+function receivePosts(subreddit, json) {
+    return {
+        type: RECEIVE_POSTS,
+        subreddit,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
+    }
+}
+
+// Meet our first thunk action creator!
+// Though its insides are different, you would use it just like any other action creator:
+// store.dispatch(fetchPosts('reactjs'))
+
+function fetchPosts(subreddit) {
+
+    // Thunk middleware knows how to handle functions.
+    // It passes the dispatch method as an argument to the function,
+    // thus making it able to dispatch actions itself.
+
+    return function (dispatch) {
+
+        // First dispatch: the app state is updated to inform
+        // that the API call is starting.
+
+        dispatch(requestPosts(subreddit));
+
+        // The function called by the thunk middleware can return a value,
+        // that is passed on as the return value of the dispatch method.
+
+        // In this case, we return a promise to wait for.
+        // This is not required by thunk middleware, but it is convenient for us.
+
+        return fetch(`http://www.reddit.com/r/${subreddit}.json`)
+            .then(response => response.json())
+            .then(json =>
+
+                // We can dispatch many times!
+                // Here, we update the app state with the results of the API call.
+
+                dispatch(receivePosts(subreddit, json))
+            );
+
+        // In a real world app, you also want to
+        // catch any error in the network call.
+    }
+}
+
+//................................
+
+function selectedSubreddit(state = 'reactjs', action) {
+    switch (action.type) {
+        case SELECT_SUBREDDIT:
+            return action.subreddit;
+        default:
+            return state
+    }
+}
+
+function posts(state = {
+    isFetching: false,
+    didInvalidate: false,
+    items: []
+}, action) {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+            return Object.assign({}, state, {
+                didInvalidate: true
+            });
+        case REQUEST_POSTS:
+            return Object.assign({}, state, {
+                isFetching: true,
+                didInvalidate: false
+            });
+        case RECEIVE_POSTS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                didInvalidate: false,
+                items: action.posts,
+                lastUpdated: action.receivedAt
+            });
+        default:
+            return state
+    }
+}
+
+function postsBySubreddit(state = {}, action) {
+    switch (action.type) {
+        case INVALIDATE_SUBREDDIT:
+        case RECEIVE_POSTS:
+        case REQUEST_POSTS:
+            return Object.assign({}, state, {
+                [action.subreddit]: posts(state[action.subreddit], action)
+            });
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    postsBySubreddit,
+    selectedSubreddit
+});
+
+
+
+
+
+///......
+
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunk, // lets us dispatch() functions
+        loggerMiddleware // neat middleware that logs actions
+    )
+);
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPosts('reactjs')).then(() =>
+    console.log(store.getState())
+);
+
+
+/*
+
+const initialState = 13;
+
+const store = createStore(reducer, initialState);
+
+const render = () => {
+    console.log(`render state: ${JSON.stringify(store.getState())}`)
+};
+
+render();
+store.subscribe(render);
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'INCREMENT'});
+setTimeout(() => {
+    store.dispatch({type: 'DECREMENT'});
+}, 2000);
+
+*/
