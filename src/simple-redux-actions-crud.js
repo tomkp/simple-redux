5035801399e1d97d853aab88090c172310147f30
tomkp <tom@tomkp.com>
2016-06-23 09:48:55 +0100

    ...

---
+++ src/simple-redux-actions-crud.js
@@ -0,0 +1,49 @@
+const {createStore} = require('redux');
+const {createAction} = require('redux-actions');
+
+const add = createAction('ADD');
+const edit = createAction('EDIT');
+const remove = createAction('REMOVE');
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return [
+                ...state,
+                action.payload.data
+            ];
+        case 'EDIT':
+            return [
+                ...state.slice(0, action.payload.index),
+                action.payload.data,
+                ...state.slice(action.payload.index + 1)
+            ];
+        case 'REMOVE':
+            return [
+                ...state.slice(0, action.payload.index),
+                ...state.slice(action.payload.index + 1)
+            ];
+        default:
+            return state
+    }
+};
+
+const initialState = [];
+
+const store = createStore(reducer, initialState);
+
+const render = () => {
+    console.log(`render state: ${JSON.stringify(store.getState())}`)
+};
+
+render();
+store.subscribe(render);
+
+store.dispatch(add({data: 'TOM'}));
+store.dispatch(add({data: 'DICK'}));
+store.dispatch(add({data: 'HARRY'}));
+store.dispatch(edit({index: 1, data: 'RICHARD'}));
+store.dispatch(remove({index: 2}));
+store.dispatch(remove({index: 0}));
+
+
