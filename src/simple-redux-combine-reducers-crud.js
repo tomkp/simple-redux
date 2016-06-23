f4407aadcf12cabce25404ea6f2c4c236400fd65
tomkp <tom@tomkp.com>
2016-06-23 09:32:39 +0100

    more complex state

---
+++ src/simple-redux-combine-reducers-crud.js
@@ -0,0 +1,55 @@
+const {createStore} = require('redux');
+
+const add = (data) => {return {type: 'ADD', payload: { data: data}}};
+const edit = (index, data) => {return {type: 'EDIT', payload: { data: data, index: index}}};
+const remove = (index) => {return {type: 'REMOVE', payload: { index: index}}};
+
+
+const itemsReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return [
+                ...state,
+                action.payload.data
+            ];
+        case 'EDIT':
+            return [
+                ...state.slice(0, action.payload.index),
+                action.payload.data,
+                ...state.slice(action.payload.index + 1)
+            ];
+        case 'REMOVE':
+            return [
+                ...state.slice(0, action.payload.index),
+                ...state.slice(action.payload.index + 1)
+            ];
+        default:
+            return state
+    }
+};
+
+const reducer = (state = {}, action) => {
+    return {
+        items: itemsReducer(state.items, action)
+    }
+}
+
+const initialState = {items:[]};
+
+const store = createStore(reducer, initialState);
+
+const render = () => {
+    console.log(`render state: ${JSON.stringify(store.getState())}`)
+};
+
+render();
+store.subscribe(render);
+
+store.dispatch(add('TOM'));
+store.dispatch(add('DICK'));
+store.dispatch(add('HARRY'));
+store.dispatch(edit(1, 'RICHARD'));
+store.dispatch(remove(2));
+store.dispatch(remove(0));
+
+
