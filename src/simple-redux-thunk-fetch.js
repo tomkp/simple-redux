e882680411c31ae77029c1d9288ca0733e958749
tomkp <tom@tomkp.com>
2016-06-29 09:29:58 +0100

    ...

--- src/simple-redux-thunk-fetch.js
+++ src/simple-redux-thunk-fetch.js
@@ -27,7 +27,7 @@ const fetchQuote = (symbol) => {
         dispatch(fetchRequest());
         return fetch(`http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.quotes+where+symbol+in+("${symbol}")&format=json&env=store:%2F%2Fdatatables.org%2Falltableswithkeys`)
             .then(res => res.json())
-            .then(json => dispatch(fetchSuccess(json.body)))
+            .then(json => dispatch(fetchSuccess(json)))
             .catch(ex => dispatch(fetchFailure(ex)))
     }
 };
@@ -35,16 +35,27 @@ const fetchQuote = (symbol) => {
 const reducer = (state = {}, action) => {
     switch (action.type) {
         case 'FETCH_REQUEST':
+            return Object.assign({}, state, {
+                status: action.type
+            });
         case 'FETCH_SUCCESS':
-        case 'FETCH_FAILURE': return Object.assign({}, state, {
-            status: action.type
-        });
-        default: return state;
+            return Object.assign({}, state, {
+                status: action.type,
+                quote: action.body.query.results.quote.Ask,
+                error: undefined
+            });
+        case 'FETCH_FAILURE':
+            return Object.assign({}, state, {
+                status: action.type,
+                quote: undefined,
+                error: action.ex.message
+            });
+        default:
+            return state;
     }
 };
+
 const store = createStore(reducer, applyMiddleware(thunk));
-
 store.subscribe(() => console.log(`render state: ${JSON.stringify(store.getState())}`));
-
-
 store.dispatch(fetchQuote('TSCO.L'));
+store.dispatch(fetchQuote('XXX&'));
